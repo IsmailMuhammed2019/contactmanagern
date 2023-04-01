9797b1f90fe56e786c6b66705a25592bd615e720
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-01 12:24:57 +0100

    import components files to app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Header from './components/Header'
+import AddContact from './components/AddContact'
+import ContactCard from './components/ContactCard'
+import ContactList from './components/ContactList'
 
 const App = () => {
   return (
