bc5ef02772fd42fccb7be5ca16e3b1e737a05e0d
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-01 23:55:56 +0100

    style contact list display

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -4,15 +4,16 @@ const ContactList = (props) => {
         const {contacts} = props
   return (
     <div className='ui celled list'>
+      <h2 style={{marginTop:'2rem'}}>Contact List</h2>
     {contacts.map((contact) => {
         const {id, name, email} = contact
         return(
-            <div className='item' key={id}>
-            <div className='content'>
+            <div className='item' key={id} style={{display: 'table', width: '100%'}}>
+            <div className='content' style={{marginTop:'1rem', marginBottom:'1rem'}}>
                 <div className='header'>{name}</div>
                 <div>{email}</div>
             </div>
-            <i className='trash alternate outline icon'></i>
+            <i className='trash alternate outline icon red' style={{verticalAlign: 'middle'}}></i>
         </div>
         )
     })
