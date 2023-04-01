dc85bfbe366c54d813669686a4a02148ed7ca6d0
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-01 14:10:54 +0100

    create add contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import AddContact from './components/AddContact'
-import ContactCard from './components/ContactCard'
+// import ContactCard from './components/ContactCard'
 import ContactList from './components/ContactList'
 
 const App = () => {
+  const [contacts, setContacts] = useState([{id: 1, name: 'Ismail Muhammed', email: 'imuhammed@gmail.com'}, {id: 2, name: 'Opeyemi Omojoro', email: 'oomojoro@gmail.com'}])
   return (
-    <div>
+    <div className='ui container'>
       <Header />
+      <AddContact />
     </div>
   )
 }
