35cbfdb76fe3016041dce601b02500f29380e6cc
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-01 23:24:38 +0100

    style header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 const Header = () => {
+
+  const headerStyle ={
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '2rem',
+    marginBottom: '2rem',
+    textTransform: 'uppercase'
+  }
+
   return (
     <div className='ui fixed menu'>
         <div className='ui container center'>
-            <h2>Contact Manager</h2>
+            <h2 style={headerStyle}>Contact Manager</h2>
         </div>
     </div>
   )
