5b7952ec556c18470e71843d622837b7a63d959d
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-02 00:20:59 +0100

    destruct props to maintain contact properties

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const ContactCard = () => {
+const ContactCard = (props) => {
+    const {id, name, email} = props.contact
   return (
     <div className='item' key={id} style={{display: 'table', width: '100%'}}>
             <div className='content' style={{marginTop:'1rem', marginBottom:'1rem'}}>
