b48fe9bed2d2492917c7995d4f29a96708c5ddcd
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-05 15:36:51 +0100

    update id for users

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactCard = (props) => {
     const {id, name, email} = props.contact
   return (
-    <div className='item' key={id} style={{display: 'table', width: '100%'}}>
+    <div className='item' style={{display: 'table', width: '100%'}}>
             <div className='content' style={{marginTop:'1rem', marginBottom:'1rem'}}>
                 <div className='header'>{name}</div>
                 <div>{email}</div>
