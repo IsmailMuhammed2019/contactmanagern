bbc866c7b791de267df9332f00466ec7d6caefdd
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-05 15:36:37 +0100

    clean function for delete

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
-const AddContact = () => {
+const AddContact = ({addContact, handleChange}) => {
+
   return (
-    <div className='ui main'>
+    <div className='ui main' style={{marginTop:'8rem'}}>
         <h2>Add Contact</h2>
-        <form className='ui form'>
+        <form className='ui form' onSubmit={addContact}>
             <div className='field'>
                 <label>Name</label>
-                <input type='text' name='name' placeholder='Name' />
+                <input type='text' name='name' placeholder='Name' value={name} onChange={() => handleChange(e)}/>
             </div>
             <div className='field'>
                 <label>Email</label>
-                <input type='text' name='email' placeholder='Email' />
+                <input type='text' name='email' placeholder='Email' value={email} onChange={() => handleChange(e)}/>
             </div>
-            <button className='ui button blue'>Add Contact</button>
+            <button className='ui button blue' type='submit'>Add Contact</button>
         </form>
     </div>
   )
