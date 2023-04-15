1ca0d509421555c1bd8ae97ed2fd1b939f476901
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-15 16:10:09 +0100

    declare usestate for person and list

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import AddContact from './AddContact'
 import ContactList from './ContactList'
 
 const ContactCard = () => {
+  const [person, setPerson] = useState({name: '', email: ''})
+  const [people, setPeople] = useState([])
   return (
     <div>
       <AddContact />
