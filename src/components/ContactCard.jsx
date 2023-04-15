6961dc65534761b7597a725b3521ec69ca8e1e43
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-15 16:12:39 +0100

    user adding contact list

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,11 +3,11 @@ import AddContact from './AddContact'
 import ContactList from './ContactList'
 
 const ContactCard = () => {
-  const [person, setPerson] = useState({name: '', email: ''})
-  const [people, setPeople] = useState([])
+
   return (
     <div>
       <AddContact />
+      <ContactList />
     </div>
   )
 }
