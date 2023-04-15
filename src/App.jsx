5647835b4e8a8457b539ab43368e6bdd7738155c
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-15 14:19:23 +0100

    import header to app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Header from './components/Header'
 import ContactCard from './components/ContactCard'
 
 const App = () => {
   return (
     <main className='ui container'>
+        <Header />
         <ContactCard />
+
     </main>
   )
 }
