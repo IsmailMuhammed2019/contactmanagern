e204f30f3ded154f65354063682c7fcd00c1434c
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-15 16:03:59 +0100

    add form and form field

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const AddContact = () => {
   return (
-    <div>AddContact</div>
+    <div className='ui main'>
+      <form className='ui form'>
+        <div className='field'>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name='name' id='name' value='name' />
+        </div>
+        <div className='field'>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name='name' id='name' value='name' />
+        </div>
+        <button type='submit' className='ui button blue'>Add</button>
+      </form>
+
+    </div>
   )
 }
 
