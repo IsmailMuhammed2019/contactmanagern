bb3fc136edba6eb719df7da714ded2444ed8c854
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-15 16:19:58 +0100

    handle input event with onchange hook

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -2,16 +2,22 @@ import React, {useState} from 'react'
 
 const AddContact = () => {
   const [person, setPerson] = useState({name: '', email: ''})
+  
+  const handleChange = (e) => {
+    e.preventDefault()
+    setPerson(e.target.value)
+  }
+
   return (
     <div className='ui main'>
       <form className='ui form'>
         <div className='field'>
         <label htmlFor="name">Name:</label>
-        <input type="text" name='name' id='name' value='name' />
+        <input type="text" name='name' id='name' value={person.name} onChange={handleChange} />
         </div>
         <div className='field'>
         <label htmlFor="email">Email:</label>
-        <input type="text" name='email' id='name' value='name' />
+        <input type="text" name='email' id='name' value={person.name} onChange={handleChange} />
         </div>
         <button type='submit' className='ui button blue'>Add</button>
       </form>
