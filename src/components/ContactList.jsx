2f074aeb9ba2b86e7974d96bae617986c15674e9
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-15 16:13:34 +0100

    correct statement for contactlist state

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
-const ContactList = () => {
+const ContactList = () => {  
+  const [people, setPeople] = useState([])
   return (
     <div>ContactList</div>
   )
