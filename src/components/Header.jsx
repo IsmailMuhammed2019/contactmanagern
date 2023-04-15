0f908d3517df6689bb7e26c9ed5b2e83e188886d
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-15 15:58:41 +0100

    style header text to fit content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const Header = () => {
+
+  const headerStyle ={
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '2rem',
+    marginBottom: '2rem',
+    textTransform: 'uppercase',
+    width:'fit-content',
+  }
   return (
     <header className='ui fix menu'>
-      <div className='ui container center'>
+      <div className='ui container center' style={headerStyle}>
         <h2>Contact Manager</h2>
       </div>
     </header>
