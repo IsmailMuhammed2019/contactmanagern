8f0f131bde8118bedf92d3c72295f11d03813e5d
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-15 17:36:31 +0100

    style list and icon

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,9 +1,24 @@
 import React, {useState} from 'react'
 
 
-const ContactList = () => {  
+const ContactList = ({contacts}) => {  
+  
   return (
-    <div>ContactList</div>
+    <div className='ui celled list'>
+      <h3>Contact List</h3>
+      {contacts.map((contact) => {
+        const {id, name, email} = contact
+        return (
+          <div key={id} className='item' >
+            <div className='content'  >
+            <div className='header'>{name}</div>
+            <div>{email}</div>
+            </div>
+            <i className='trash alternate red outline icon'></i>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
