ab67ad2f6ead50f45b0819cf1eda90e3e2b117c1
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-17 09:49:28 +0100

    change button color

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const AddContact = () => {
   return (
-    <div>AddContact</div>
+    <div className='ui main'>
+<form className='ui form'>
+  <div className='field'>
+    <label htmlFor="name">Name:</label>
+    <input type="text" name='name' id='name' />
+  </div>
+  <div className='field'>
+    <label htmlFor="email">Email:</label>
+    <input type="text" name='email' id='email' />
+  </div>
+  <button className='ui button green' type='submit'>Add Contact</button>
+</form>
+    </div>
   )
 }
 
