8c9a1898ff70ba637d669ce42d833d48317f009a
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-20 09:35:11 +0100

    create delete funcction

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Header from './components/Header'
+import AddContact from './components/AddContact'
+import ContactList from './components/ContactList' 
 
 const App = () => {
+    const [contacts, setContacts] = useState(() => {
+        const savedContacts = localStorage.getItem('contacts')
+        const initialValue = JSON.parse(savedContacts)
+        return initialValue || ''
+    }) 
+
+    const addPerson = (contact) => {
+        if(contact == null) return
+        setContacts([...contacts, {id: new Date().getTime().toString(), ...contacts}])
+    }
+
+    useEffect(() => {
+        localStorage.setItem('contacts', JSON.stringify(contacts))
+    }, [contacts])
+
+    const deleteContact = (id) => {
+        const newContacts = contacts.filter((contact) => contact.id !== id)
+        setContacts([...contacts, newContacts])
+    }
+
   return (
-    <div>App</div>
+    <>
+<Header />
+    <main className='ui container'>
+<AddContact addPerson={addPerson}/>
+<ContactList contacts={contacts} deleteContact={deleteContact}/>
+    </main>
+</>
   )
 }
 
