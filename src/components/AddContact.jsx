168d6d663e568a200882c3d5602a38f604e2a964
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-20 10:12:43 +0100

    style add contact button...

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const AddContact = () => {
   return (
-    <div>
-      
+    <div className='ui main'>
+      <h2>Add Contact</h2>
+      <div className='ui form'>
+        <div className='field'>
+          <label htmlFor="name">Name: </label>
+          <input type="text" id='name'/>
+        </div>
+        <div className='field'>
+          <label htmlFor="email">Email: </label>
+          <input type="text" id='email' />
+        </div>
+        <button className='ui button green'>Add Contact</button>
+      </div>
     </div>
   )
 }
