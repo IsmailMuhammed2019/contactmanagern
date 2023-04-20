b18ae694246af48efaef332b441b292c8b350175
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-20 09:35:36 +0100

    update contactCard props

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const ContactCard = () => {
+const ContactCard = ({contact, clickHandler}) => {
+  const {id, name, email} = contact
   return (
-    <div>ContactCard</div>
+    <div className='item' >
+            <div className='content' style={{marginTop: '0.2rem'}}>
+            <div className='header'>{name}</div>
+            <div>{email}</div>
+            </div>
+            <i className='trash alternate outline icon red' style={{cursor: 'pointer', float: 'right', marginBottom: '0.2rem'}} onClick={() => clickHandler(id)}></i>
+          </div>
   )
 }
 
