c2dceb1de22d3aed63a1476ca1a2baeae05b00fc
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-20 11:11:44 +0100

    correct margins for icon

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -4,17 +4,17 @@ const ContactList = () => {
   const [contacts, setContacts] = useState([{id: 1, name: 'Ismail Muhammed', email: 'ismail2019@gmail.com' }, {id: 2, name: 'Ahmed Chambers', email: 'chambers@gmail.com'}])
 
   return (
-    <div>
+    <div className='ui celled list'>
       <h2 style={{marginTop: '3rem'}}>Contact List</h2>
         {contacts.map((contact) => {
           const {id, name, email} = contact
           return(
-            <div className='item' key={id} style={{display: 'flex', alignItems: 'center', justifyContent:'space-between'}}>
+            <div className='ui item' key={id} style={{display: 'flex', alignItems: 'center', justifyContent:'space-between'}}>
             <div className='content'>
               <div className='header'>{name}</div>
               <div>{email}</div>
             </div>
-            <i className='trash alternate outline icon red' ></i>
+            <i className='trash alternate outline icon red' style={{cursor:'pointer'}}></i>
             </div>
           )
         })}
