03afec5654fa3198f0944672252b22ed69e383e4
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-20 09:35:58 +0100

    clean unused functions

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ContactCard from './ContactCard'
 
-const ContactList = () => {
+const ContactList = ({contacts, deleteContacts}) => {
+
+  const clickHandler =(id) => {
+    deleteContacts(id)
+  }
   return (
-    <div>ContactList</div>
+    <div className='ui celled list' style={{marginTop: '3rem'}}>
+      <h2 style={{marginBottom: '1rem'}}>Contact List</h2>
+      {contacts.map((contact) => {
+        return (
+          <ContactCard key={contact.id} contact={contact} clickHandler={clickHandler}/>
+        )
+      })}
+    </div>
   )
 }
 
